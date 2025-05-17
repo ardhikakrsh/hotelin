4f8ef0261992d9e186a776a8e3dfb4efa534e0d8
ardhikakrsh <ardhikakrishna@gmail.com>
2025-05-17 18:43:23 +0700

    feat: update models

--- lib/models/rooms.dart
+++ lib/models/rooms.dart
@@ -1,9 +1,11 @@
 class Room {
+  final int id;
   final String roomType; // e.g., Reguler, Deluxe
   final String bedType; // e.g., Single Bed, Double Bed
   final int price;
 
   Room({
+    required this.id,
     required this.roomType,
     required this.bedType,
     required this.price,
